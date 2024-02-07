eba072c2ab3a0060e60e1e791548f794e95dbb6c
rashedmbstucse07 <rashedmbstucse07@gmail.com>
2024-02-08 00:02:14 +0600

    added Encapsulation oop feature

---
+++ src/javaoopfeatures/encapsulation/Encapsulation.java
@@ -0,0 +1,41 @@
+package javaoopfeatures.encapsulation;
+
+public class Encapsulation {
+    // private variables those are only accessible by getter & Setter method
+    private String name;  // encapsulated name variable by private access modifier
+    private String id;
+    private int age;
+
+    //Set name by setName method
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Get  Name by getName method
+    public String getName() {
+        return name;
+    }
+
+    //Set Age by setAge method
+    public void setAge(int Age) {
+        age = Age;
+    }
+
+    //Get  Age by getAge method
+    public int getAge() {
+        return age;
+    }
+
+    //Set id by setId method
+    public void setId(String Id) {
+        id = Id;
+    }
+
+    //Get Id by getId method
+    public String getId() {
+        return id;
+    }
+
+
+
+}
---
+++ src/javaoopfeatures/encapsulation/EncapsulationTest.java
@@ -0,0 +1,9 @@
+package javaoopfeatures.encapsulation;
+
+public class EncapsulationTest {
+    public static void main(String[] args) {
+        Encapsulation encapsulation = new Encapsulation();
+        encapsulation.setName("Rashed");
+        System.out.println("Name is:" + " " + encapsulation.getName());
+    }
+}
