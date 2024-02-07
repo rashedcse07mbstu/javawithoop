3042bae3c7d598f8452d14c047cf6fa70371728e
rashedmbstucse07 <rashedmbstucse07@gmail.com>
2024-02-07 23:40:22 +0600

    added Method Overloading oop feature

---
+++ src/javaoopfeatures/MethodOverloading.java
@@ -0,0 +1,38 @@
+package javaoopfeatures;
+
+public class MethodOverloading {
+    //Write a program for Method Overloading oop feature
+
+    //Method not take parameter & not return any value
+    public void doSum() {
+        System.out.println("SQA Learning Academy");
+    }
+
+    //Method take parameter & not return any value
+    public void doSum(int x, int y) {
+        System.out.println(x + y);
+    }
+
+    //Method take parameter &  return ny value
+    public void doSum(int x, float y) {
+        System.out.println(x + y);
+    }
+
+    //Method not take parameter & not return ny value
+    public void doSum(int x, float y, int z) {
+        System.out.println(x + y + z);
+    }
+
+    //Method take parameter & return value
+    public int doSum(int num1, int num2, int num3) {
+        int resultOfSum = num1 + num2; //resultOfSum is a Local variable
+        return resultOfSum;
+    }
+
+    //Method take parameter & return value
+    public float doSum(float num1, float num2) {
+        float resultOfSum = num1 + num2;
+        return resultOfSum;
+    }
+
+}
---
+++ src/javaoopfeatures/MethodOverloadingTest.java
@@ -0,0 +1,13 @@
+package javaoopfeatures;
+
+public class MethodOverloadingTest {
+    public static void main(String[] args) {
+        MethodOverloading methodOverloading= new MethodOverloading();
+        methodOverloading.doSum();
+        methodOverloading.doSum(10, 290);
+        methodOverloading.doSum(10, 10.40f);
+        methodOverloading.doSum(10, 10.30f, 10);
+        methodOverloading.doSum(10, 10, 10);
+        methodOverloading.doSum(10.5f, 10.5f);
+    }
+}
